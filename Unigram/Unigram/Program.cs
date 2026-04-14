406424239b9bcb7f97475d23cb4cb483dab1f700
SimboMC <solo.simbobs@gmail.com>
2026-04-14 15:17:16 +0200

    initl

---
+++ Unigram/Unigram/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: noepe
+ * Date: 14.04.2026
+ * Time: 15:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Unigram
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
---
+++ Unigram/Unigram/Unigram.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: noepe
+ * Date: 14.04.2026
+ * Time: 15:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Unigram
+{
+	/// <summary>
+	/// Description of Unigram.
+	/// </summary>
+	public class Unigram
+	{
+		public Unigram()
+		{
+		}
+	}
+}
