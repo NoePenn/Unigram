04ed970d920cbc3f6cfc46f4266e499d1cc6d8eb
f1cht3nN4d3l <jakobhilpold29@gmail.com>
2026-04-14 16:10:03 +0200

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace Unigram
+{
+	/// <summary>
+	/// Gasser muas do no inni kochn
+	/// </summary>
+	public class Graph {
+				
+		public Graph(Color color, Transformer transformer, Analysis analysis) {
+			this.Color = color;
+			this.Transformer = transformer;
+			
+		}
+		/// <summary>
+		/// Gasser muas do no inni kochn
+		/// </summary>
+		Color Color { get; set; }
+		/// <summary>
+		/// Gasser muas do no inni kochn
+		/// </summary>
+		Transformer Transformer { get; set; }
+		/// <summary>
+		/// Gasser muas do no inni kochn
+		/// </summary>
+		Analysis Analysis { get; } 
+		
+		public void AddPoint(PointF point, float viewPortHeight, float viewPortWidth) {
+			
+		}
+	}
+}
