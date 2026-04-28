1656b7075c49c7ac953c62dc53bf41c209197f9a
f1cht3nN4d3l <jakobhilpold29@gmail.com>
2026-04-28 15:29:40 +0200

    Graph update

--- Unigram/Unigram/Graph.cs
+++ Unigram/Unigram/Graph.cs
@@ -1,22 +1,44 @@
-﻿/*
- * Created by SharpDevelop.
- * User: noepe
- * Date: 14.04.2026
- * Time: 15:21
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
 using System;
+using System.Drawing;
+using System.Collections.Generic;
 
 namespace Unigram
 {
 	/// <summary>
-	/// Description of Graph.
+	/// Represents a visual graph object that manages geometric transformation and analysis of plotted points.
 	/// </summary>
-	public class Graph
-	{
-		public Graph()
-		{
+	public class Graph {
+		List <PointF> points = new List<PointF>();
+		List <PointF> plotPoints = new List<PointF>();
+		Analysis analysis;
+		
+		public Graph(Color color, Analysis analysis) {
+			this.Color = color;
+			this.analysis = analysis;
+		}	
+		
+		/// <summary>
+		/// Gets or sets the color used to render the graph visualization.
+		/// </summary>
+		public Color Color { get; set; }
+		/// <summary>
+		/// Gets the analysis object that provides computational analysis of the plotted graph data.
+		/// </summary>
+		public Analysis Analysis { get {return analysis; } }
+		
+		public List<PointF> Points { get { return this.points; } }
+		public List<PointF> PlotPoints { get { return this.plotPoints; } }
+		
+		
+		/// <summary>
+		/// Adds a point to the graph and transforms it according to the viewport dimensions.
+		/// </summary>
+		/// <param name="point">The point coordinates to add to the graph.</param>
+		public void AddPoint(PointF point, Transformer transformer) {
+			points.Add(point);
+			plotPoints.Add(transformer.MathToPixel(point));
+			Analysis.AddPoint(point);
+			
 		}
 	}
 }
