4abeda440757f1f64270cb6f9e110fedbeb603a9
4xilas <al.alex.decarli@gmail.com>
2026-04-28 14:43:19 +0200

    Update Transformer.cs

--- Unigram/Unigram/Transformer.cs
+++ Unigram/Unigram/Transformer.cs
@@ -7,54 +7,43 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-// using System.Drawing;
+using System.Drawing;
 
 namespace Unigram
 {
 	/// <summary>
-	/// This class can convert mathematical points to pixel coordinates and viceversa, every time the coordinate system changes, UpdateTransformer() MUST be called.
+	/// Description of Transformer.
 	/// </summary>
 	public class Transformer
 	{
-	    int VPHeight { get; set; }
-		int VPWidth { get; set; }
 		
-		/// <summary>
-		/// Is the X-axis logarithmic?
-		/// </summary>
-		bool XLog { get; set; }
-		/// <summary>
-		/// Is the Y-axis logarithmic?
-		/// </summary>
-		bool YLog { get; set; }
-		
-		
-		
-		/// <summary>
-		/// Returns the lowest value for X (at the bottom of the graph) 
-		/// </summary>
-		float XMin { get; set; }
-		/// <summary>
-		/// Returns the maximum value for X (at the top of the graph)
-		/// </summary>
-		float XMax { get; set; }
-		
-		/// <summary>
-		/// Returns the lowest value for Y (at the left of the graph) 
-		/// </summary>
-		float YMin { get; set; }
-		/// <summary>
-		/// Returns the maximum value for Y (at the right of the graph)
-		/// </summary>
-		float YMax { get; set; }
+	    public int VPHeightPX { get; set; }
+	    public int VPWidthPX  { get; set; }
+	    public bool XLog    { get; set; }
+	    public bool YLog    { get; set; }
+	    public float XMin   { get; set; }
+	    public float XMax   { get; set; }
+	    public float YMin   { get; set; }
+	    public float YMax   { get; set; }
 	
 	    // Precomputed in UpdateTransformer()
 	    private float _scaleX, _scaleY, _logXMin, _logYMin;
-	
+		
+	    Transformer(){
+	    	XLog = false;
+	    	YLog = false;
+	    	
+	    	XMin = 	5;
+	    	XMax = 5;
+	    	
+	    	YMin = 	5;
+	    	YMax = 5;
+		}
+	    
 	    public void UpdateTransformer()
 	    {
-	        _scaleX  = XLog ? VPWidth  / (float)Math.Log(XMax / XMin) : VPWidth  / (XMax - XMin);
-	        _scaleY  = YLog ? VPHeight / (float)Math.Log(YMax / YMin) : VPHeight / (YMax - YMin);
+	        _scaleX  = XLog ? VPWidthPX  / (float)Math.Log(XMax / XMin) : VPWidthPX  / (XMax - XMin);
+	        _scaleY  = YLog ? VPHeightPX / (float)Math.Log(YMax / YMin) : VPWidthPX / (YMax - YMin);
 	        _logXMin = XLog ? (float)Math.Log(XMin) : 0;
 	        _logYMin = YLog ? (float)Math.Log(YMin) : 0;
 	    }
@@ -63,18 +52,7 @@ namespace Unigram
 	    {
 	        float u = XLog ? ((float)Math.Log(point.X) - _logXMin) * _scaleX : (point.X - XMin) * _scaleX;
 	        float v = YLog ? ((float)Math.Log(point.Y) - _logYMin) * _scaleY : (point.Y - YMin) * _scaleY;
-	        return new PointF(u, VPHeight - v);
+	        return new PointF(u, VPHeightPX - v);
 	    }
-	    
-	    public PointF PixelToMath(PointF pixel)
-		{
-		    float u = pixel.X / _scaleX;
-		    float v = (VPHeight - pixel.Y) / _scaleY;
-		
-		    float x = XLog ? (float)Math.Exp(u + _logXMin) : u + XMin;
-		    float y = YLog ? (float)Math.Exp(v + _logYMin) : v + YMin;
-		
-		    return new PointF(x, y);
-		}
 	}
 }
