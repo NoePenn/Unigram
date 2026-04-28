7f7a3d4bf4fdb6ef5e5713edb38498dae7419f8e
4xilas <al.alex.decarli@gmail.com>
2026-04-28 15:30:50 +0200

    Update Transformer.cs

--- Unigram/Unigram/Transformer.cs
+++ Unigram/Unigram/Transformer.cs
@@ -16,7 +16,6 @@ namespace Unigram
 	/// </summary>
 	public class Transformer
 	{
-		
 	    public int VPHeightPX { get; set; }
 	    public int VPWidthPX  { get; set; }
 	    public bool XLog    { get; set; }
@@ -26,9 +25,7 @@ namespace Unigram
 	    public float YMin   { get; set; }
 	    public float YMax   { get; set; }
 	
-	    // Precomputed in UpdateTransformer()
-	    private float _scaleX, _scaleY, _logXMin, _logYMin;
-		
+	    
 	    public Transformer(){
 	    	XLog = false;
 	    	YLog = false;
@@ -40,19 +37,29 @@ namespace Unigram
 	    	YMax = 5;
 		}
 	    
-	    public void UpdateTransformer()
-	    {
+	    // Precomputed in UpdateTransformer()
+	    private float _scaleX, _scaleY, _logXMin, _logYMin;
+	    public void UpdateTransformer(){
 	        _scaleX  = XLog ? VPWidthPX  / (float)Math.Log(XMax / XMin) : VPWidthPX  / (XMax - XMin);
 	        _scaleY  = YLog ? VPHeightPX / (float)Math.Log(YMax / YMin) : VPHeightPX / (YMax - YMin);
 	        _logXMin = XLog ? (float)Math.Log(XMin) : 0;
 	        _logYMin = YLog ? (float)Math.Log(YMin) : 0;
 	    }
 	
-	    public PointF MathToPixel(PointF point)
-	    {
+	    public PointF MathToPixel(PointF point){
 	        float u = XLog ? ((float)Math.Log(point.X) - _logXMin) * _scaleX : (point.X - XMin) * _scaleX;
 	        float v = YLog ? ((float)Math.Log(point.Y) - _logYMin) * _scaleY : (point.Y - YMin) * _scaleY;
 	        return new PointF(u, VPHeightPX - v);
 	    }
+	    
+	    public PointF PixelToMath(PointF pixel){
+		    float u = pixel.X / _scaleX;
+		    float v = (VPHeightPX - pixel.Y) / _scaleY;
+		
+		    float x = XLog ? (float)Math.Exp(u + _logXMin) : u + XMin;
+		    float y = YLog ? (float)Math.Exp(v + _logYMin) : v + YMin;
+		
+		    return new PointF(x, y);
+		}
 	}
 }
