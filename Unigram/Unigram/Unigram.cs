49efe6efb485f796ee69b6cb056c6b0146a32f4f
1uigii <1uigi@protonmail.com>
2026-05-05 15:03:40 +0200

    Update Unigram.cs

--- Unigram/Unigram/Unigram.cs
+++ Unigram/Unigram/Unigram.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Created by SharpDevelop.
  * User: noepe
  * Date: 14.04.2026
@@ -7,6 +7,8 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace Unigram
 {
@@ -15,8 +17,31 @@ namespace Unigram
 	/// </summary>
 	public class Unigram
 	{
+		Viewport viewport;
+		List<Graph> graphs;
+		Transformer transformer;
+
 		public Unigram()
 		{
+		}
+
+		void UpdateTransformerPixel(int width, int height) {
+			if (transformer.VPWidthPX != width || transformer.VPHeightPX != width) {
+				transformer.VPWidthPX = width;
+				transformer.VPHeightPX = height;
+				transformer.UpdateTransformer();
+				for (int i = 0; i < graphs.Count; i++) {
+					this.graphs[i].UpdatePlot(this.transformer);
+				}
+			}
+		}
+
+		void Paint(int width, int height, Graphics g) {
+			this.UpdateTransformerPixel(width, height);
+			this.viewport.PaintCoordinateSystem(transformer, g);
+			for (int i = 0; i < graphs.Count; i++)  {
+				this.viewport.PaintGraph(this.graphs[i]);
+			}
 		}
 	}
 }
