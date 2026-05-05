564174762ad49908c0e4ef945cb7a39fce1bc0ee
1uigii <1uigi@protonmail.com>
2026-05-05 16:06:19 +0200

    Refactor Viewport class and update coordinate system

--- Unigram/Unigram/Viewport.cs
+++ Unigram/Unigram/Viewport.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Created by SharpDevelop.
  * User: noepe
  * Date: 14.04.2026
@@ -17,181 +17,57 @@ namespace Unigram
 	/// </summary>
 	public class Viewport
 	{
-		
-		public void PaintCoordinateSystem(Transformer transformer, Graphics g){
-			
-			float scaleY = transformer.VPHeightPX; 
-			float scaleX = transformer.VPWidthPX;
-			
-			float linesOffset = 10f;
-			float viewportOffsetX = scaleX/2;
-			float viewportOffsetY = scaleY/2;
-			
-			Pen penMiddle = new Pen(Color.Black, 4);
-			Pen penBackgroundThick = new Pen(Color.Gray, 2);
-			Pen penBackgroundThin = new Pen(Color.Gray, 1);
-			Pen functionPen = new Pen(Color.Red, 3);
-			Font axisFont = new Font("Arial", 12);	
+		float MajorX { get; set; }
+		float MajorY { get; set; }
+		float MinorX { get; set; }
+		float MinorY { get; set; }
+
+		public Viewport() {
+			MajorX = 1;
+			MajorY = 1;
+			MinorX = 0.25f;
+			MinorY = 0.25f;
+		}
+
+		public void PaintCoordinateSystem(Transformer transformer, Graphics g) {
+			Font axisFont = new Font(FontFamily.GenericMonospace, 12);	
             Brush textBrush = Brushes.Black;
-			
-            
-            
-            float centerX = viewportOffsetX; //how far away the Middle lines are drawn from Origin
-            float centerY = viewportOffsetY;
-            
-			int labelStep = linesOffset > 30 ? 1 : linesOffset > 15 ? 2 : 5; //If zoomed into Graph via Trackbar more lines appear
-			
-			float linesCountX = scaleX / (linesOffset); //Linesoffset should be split into X and Y soon!
-            float linesCountY = scaleY / (linesOffset);
-            
-			StringFormat centerFormat = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Far };
-			StringFormat rightFormat = new StringFormat { Alignment = StringAlignment.Far, LineAlignment = StringAlignment.Center };
-			
-			// Draw central horizontal axis (thick black)
-			g.DrawLine(penMiddle, 0, centerY, scaleX, centerY);
-	
-			// Draw central vertical axis (thick black) 
-			g.DrawLine(penMiddle, centerX, 0, centerX, scaleY);
-				
-							
-		    for (int i = 1; i < linesCountX; i++) //draw Lines from Middle lines two the edge (thick and thin)
-				{
-			    float offsetX = linesOffset * i; //Offset from the middle Lines
-			    
-			    PointF HorizontalPoint1 = new PointF(0, centerY + offsetX);
-			    PointF HorizontalPoint2 = new PointF(scaleX, centerY + offsetX);
-			    PointF HorizontalPoint3 = new PointF(0, centerY - offsetX);
-			    PointF HorizontalPoint4 = new PointF(scaleX, centerY - offsetX);
-			
-			
-			
-			    if (i % labelStep == 0) //
-			    {
-			    	g.DrawString((-i).ToString(),   axisFont, textBrush, centerX + 5, centerY + offsetX);
-					g.DrawString(i.ToString(), axisFont, textBrush, centerX + 5, centerY - offsetX);
-			
-					var pen = (i % 2) <= 0 ? penBackgroundThick : penBackgroundThin;
-			        g.DrawLine(pen, HorizontalPoint1, HorizontalPoint2);
-			        g.DrawLine(pen, HorizontalPoint3, HorizontalPoint4);
-			    }
 
+			PointF zero = transformer.MathToPixel(new PointF(0, 0));
 
-	}
-		    for (int i = 1; i < linesCountX; i++) //draw Lines from Middle lines two the edge (thick and thin)
-				{
-			    float offsetY = linesOffset * i; //Offset from the middle Lines
-			    
-			    PointF VerticalPoint1 = new PointF(centerX + offsetY, 0);
-			    PointF VerticalPoint2 = new PointF(centerX + offsetY, scaleY);
-			    PointF VerticalPoint3 = new PointF(centerX - offsetY, 0);
-			    PointF VerticalPoint4 = new PointF(centerX - offsetY, scaleY);
+			var minorPen = new Pen(Color.LightGray);
+			for (float xBar = (float)Math.Floor(transformer.XMin / this.MinorX) *  this.MinorX; xBar <= transformer.XMax; xBar += this.MinorX) {
+				float x = transformer.MathToPixel(new PointF(xBar, 1)).X;
+				g.DrawLine(minorPen, x, 0, x, transformer.VPHeightPX);
+			}
+
+			for (float yBar = (float)Math.Floor(transformer.YMin / this.MinorY) *  this.MinorY; yBar <= transformer.YMax; yBar += this.MinorY) {
+				float y = transformer.MathToPixel(new PointF(1, yBar)).Y;
+				g.DrawLine(minorPen, 0, y, transformer.VPWidthPX, y);
+			}
+
+			var majorPen = new Pen(Color.Gray);
+			for (float xBar = (float)Math.Floor(transformer.XMin / this.MajorX) *  this.MajorX; xBar <= transformer.XMax; xBar += this.MajorX) {
+				float x = transformer.MathToPixel(new PointF(xBar, 1)).X;
+				g.DrawLine(majorPen, x, 0, x, transformer.VPHeightPX);
+				g.DrawString("" + xBar, axisFont, textBrush, x, zero.Y);
+			}
+
+			for (float yBar = (float)Math.Floor(transformer.YMin / this.MajorY) *  this.MajorY; yBar <= transformer.YMax; yBar += this.MajorY) {
+				float y = transformer.MathToPixel(new PointF(1, yBar)).Y;
+				g.DrawLine(majorPen, 0, y, transformer.VPWidthPX, y);
+				g.DrawString("" + yBar, axisFont, textBrush, zero.X, y);
+			}
 			
-			
-			
-			    if (i % labelStep == 0) //
-			    {
-					g.DrawString(i.ToString(),   axisFont, textBrush, centerX + offsetY, centerY + 5);
-					g.DrawString((-i).ToString(), axisFont, textBrush, centerX - offsetY, centerY +5);
-					
-					var pen = (i % 2) <= 0 ? penBackgroundThick : penBackgroundThin;
-			        g.DrawLine(pen, VerticalPoint1, VerticalPoint2);
-			        g.DrawLine(pen, VerticalPoint3, VerticalPoint4);
-
-			    }
-
-
-	}
+			var zeroPen = new Pen(Color.Black);
+			g.DrawLine(zeroPen, zero.X, 0, zero.X, transformer.VPHeightPX);
+			g.DrawLine(zeroPen, 0, zero.Y, transformer.VPWidthPX, zero.Y);
 		}
 		
 		public void PaintGraph(Graph graph, Graphics g){
-			Pen penGraph = new Pen(graph.Color, 4);
+			Pen penGraph = new Pen(graph.Color, 1);
 			g.DrawCurve(penGraph, graph.PlotPoints.ToArray(), 0.5f);
 		}
-		
-		/*public void PixelCrafter(System.Windows.Forms.PaintEventArgs e,int scaleX,int scaleY, float trackBarValue)
-		{
-
-			
-			float linesOffset = trackBarValue;
-			float viewportOffsetX = scaleX/2;
-			float viewportOffsetY = scaleY/2;
-			
-			Pen penMiddle = new Pen(Color.Black, 4);
-			Pen penBackgroundThick = new Pen(Color.Gray, 2);
-			Pen penBackgroundThin = new Pen(Color.Gray, 1);
-			Pen functionPen = new Pen(Color.Red, 3);
-			Font axisFont = new Font("Arial", 12);	
-            Brush textBrush = Brushes.Black;
-			
-            
-            
-            float centerX = viewportOffsetX; //how far away the Middle lines are drawn from Origin
-            float centerY = viewportOffsetY;
-            
-			int labelStep = linesOffset > 30 ? 1 : linesOffset > 15 ? 2 : 5; //If zoomed into Graph via Trackbar more lines appear
-			
-			float linesCountX = scaleX / (linesOffset); //Linesoffset should be split into X and Y soon!
-            float linesCountY = scaleY / (linesOffset);
-            
-			StringFormat centerFormat = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Far };
-			StringFormat rightFormat = new StringFormat { Alignment = StringAlignment.Far, LineAlignment = StringAlignment.Center };
-			
-			// Draw central horizontal axis (thick black)
-			e.Graphics.DrawLine(penMiddle, 0, centerY, scaleX, centerY);
-	
-			// Draw central vertical axis (thick black) 
-			e.Graphics.DrawLine(penMiddle, centerX, 0, centerX, scaleY);
-				
-							
-		    for (int i = 1; i < linesCountX; i++) //draw Lines from Middle lines two the edge (thick and thin)
-				{
-			    float offsetX = linesOffset * i; //Offset from the middle Lines
-			    
-			    PointF HorizontalPoint1 = new PointF(0, centerY + offsetX);
-			    PointF HorizontalPoint2 = new PointF(scaleX, centerY + offsetX);
-			    PointF HorizontalPoint3 = new PointF(0, centerY - offsetX);
-			    PointF HorizontalPoint4 = new PointF(scaleX, centerY - offsetX);
-			
-			
-			
-			    if (i % labelStep == 0) //
-			    {
-			    	e.Graphics.DrawString((-i).ToString(),   axisFont, textBrush, centerX + 5, centerY + offsetX);
-					e.Graphics.DrawString(i.ToString(), axisFont, textBrush, centerX + 5, centerY - offsetX);
-			
-					var pen = (i % 2) <= 0 ? penBackgroundThick : penBackgroundThin;
-			        e.Graphics.DrawLine(pen, HorizontalPoint1, HorizontalPoint2);
-			        e.Graphics.DrawLine(pen, HorizontalPoint3, HorizontalPoint4);
-			    }
-
-
-	}
-		    for (int i = 1; i < linesCountX; i++) //draw Lines from Middle lines two the edge (thick and thin)
-				{
-			    float offsetY = linesOffset * i; //Offset from the middle Lines
-			    
-			    PointF VerticalPoint1 = new PointF(centerX + offsetY, 0);
-			    PointF VerticalPoint2 = new PointF(centerX + offsetY, scaleY);
-			    PointF VerticalPoint3 = new PointF(centerX - offsetY, 0);
-			    PointF VerticalPoint4 = new PointF(centerX - offsetY, scaleY);
-			
-			
-			
-			    if (i % labelStep == 0) //
-			    {
-					e.Graphics.DrawString(i.ToString(),   axisFont, textBrush, centerX + offsetY, centerY + 5);
-					e.Graphics.DrawString((-i).ToString(), axisFont, textBrush, centerX - offsetY, centerY +5);
-					
-					var pen = (i % 2) <= 0 ? penBackgroundThick : penBackgroundThin;
-			        e.Graphics.DrawLine(pen, VerticalPoint1, VerticalPoint2);
-			        e.Graphics.DrawLine(pen, VerticalPoint3, VerticalPoint4);
-
-			    }
-
-		    }
-		    
-			
-		}*/
 	}
 
 }
